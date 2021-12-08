6de53811b02e7e5a36bd84f17a24a2ae6bb8c883
resadent <34896462+resadent@users.noreply.github.com>
2021-12-08 15:44:27 +0100

    Update install_latest_g9.sh
    Correct typo

--- install_latest_g9.sh
+++ install_latest_g9.sh
@@ -7,13 +7,13 @@ sudo apt install libd3dadapter9-mesa libd3dadapter9-mesa:i386 libegl1-mesa libeg
 echo "Now downloading gallium nine"
 cd ~
 curl -s https://api.github.com/repos/iXit/wine-nine-standalone/releases/latest \
-| grep "gallium-nine-standalone.*tar.gz" \
+| grep "gallium-nine-standalone-v*tar.gz" \
 | cut -d : -f 2,3 \
 | tr -d \" \
 | wget -qi -
 
 echo "And installing it"
-tar -xf gallium-nine-standalone.*tar.gz
+tar -xf gallium-nine-standalone-v*tar.gz
 mv gallium-nine-standalone .gallium-nine-standalone
 cd ~/.gallium-nine-standalone
 ./nine-install.sh
