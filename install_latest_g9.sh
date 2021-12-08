46dc62c49b5fe20adede1cc89f39f57ae49928f6
resadent <34896462+resadent@users.noreply.github.com>
2021-12-08 15:43:06 +0100

    Add files via upload

---
+++ install_latest_g9.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Script to install latest gallium nine
+echo "First get the necessary packages"
+sudo apt update
+sudo apt install libd3dadapter9-mesa libd3dadapter9-mesa:i386 libegl1-mesa libegl1-mesa:i386
+
+echo "Now downloading gallium nine"
+cd ~
+curl -s https://api.github.com/repos/iXit/wine-nine-standalone/releases/latest \
+| grep "gallium-nine-standalone.*tar.gz" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -qi -
+
+echo "And installing it"
+tar -xf gallium-nine-standalone.*tar.gz
+mv gallium-nine-standalone .gallium-nine-standalone
+cd ~/.gallium-nine-standalone
+./nine-install.sh
