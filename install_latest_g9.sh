e908e27031bc3c62e0f43b0208fbbefd8eacc032
resadent <34896462+resadent@users.noreply.github.com>
2021-12-08 18:35:58 +0100

    Update install_latest_g9.sh
    Also delete gallium nine folder, since it's useless after installation.

--- install_latest_g9.sh
+++ install_latest_g9.sh
@@ -19,5 +19,6 @@ cd ~/.gallium-nine-standalone
 echo "Clean leftovers"
 rm ~/gallium-nine*.tar.gz
 rm ~/install_latest_g9.sh
+rm ~/.gallium-nine-standalone
 
 echo "And now we should be good to go :)"
